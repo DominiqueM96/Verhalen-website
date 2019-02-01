57c536b9f0441504cf0662451d4ab3434641bc8f
DominiqueM96 <45557073+DominiqueM96@users.noreply.github.com>
2019-02-01 16:47:11 +0100

    github nu wel goed uploaden?

--- js/verhaal.js
+++ js/verhaal.js
@@ -1,1 +1,33 @@
 /*jslint browser: true, devel: true, eqeq: true, plusplus: true, sloppy: true, vars: true, white: true*/
+
+var sterren = document.getElementById("sterren");
+
+sterren.addEventListener("click", function () {
+    this.children[0].style.color = "#FBDD71";
+    this.querySelector('.fas').style.fontSize = '2rem';
+    this.children[0].style.transition = 'color .5s ease 1s, font-size 0.74s ease-out';
+}, false);
+
+sterren.addEventListener("click", function () {
+    this.children[1].style.color = "#FBDD71";
+    this.querySelector('.fas').style.fontSize = '2rem';
+    this.children[1].style.transition = 'color .5s ease 1s, font-size 0.74s ease-out';
+}, false);
+
+sterren.addEventListener("click", function () {
+    this.children[2].style.color = "#FBDD71";
+    this.querySelector('.fas').style.fontSize = '2rem';
+    this.children[2].style.transition = 'color .5s ease 1s, font-size 0.74s ease-out';
+}, false);
+
+sterren.addEventListener("click", function () {
+    this.children[3].style.color = "#FBDD71";
+    this.querySelector('.fas').style.fontSize = '2rem';
+    this.children[3].style.transition = 'color .5s ease 1s, font-size 0.74s ease-out';
+}, false);
+
+sterren.addEventListener("click", function () {
+    this.children[4].style.color = "#FBDD71";
+    this.querySelector('.fas').style.fontSize = '2rem';
+    this.children[4].style.transition = 'color .5s ease 1s, font-size 0.74s ease-out';
+}, false);
